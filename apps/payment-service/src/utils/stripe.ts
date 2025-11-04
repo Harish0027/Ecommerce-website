c36843e26ad607f6bb63a434aae4fbb04fc4b2a9
Harish0027 <sutarharish143@gmail.com>
2025-11-04 13:02:59 +0530

    final changes

--- apps/payment-service/src/utils/stripe.ts
+++ apps/payment-service/src/utils/stripe.ts
@@ -3,8 +3,7 @@ dotenv.config();
 import Stripe from "stripe";
 
 const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ||
-    ("sk_test_51PRUu3D7V6SktyDSNCqoozZkRgZVj3qyqqOw7synWRvusM9aax6Wwn1c8mIaPJXDxzqxKNLp18txcnaPzjzSoeSa00Wtw7fNGn" as string),
+  process.env.STRIPE_SECRET_KEY ,
   {
     apiVersion: "2025-08-27.basil" as any,
   }
